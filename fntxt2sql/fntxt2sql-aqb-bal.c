788af041af76d7649ac6fc40a61a91d30de5b70f
Stefan Schallenberg <nafets227@users.noreply.github.com>
2018-04-26 19:13:43 +0000

    Use noted balance if booked balance is not available
    solves #1

--- fntxt2sql/fntxt2sql-aqb-bal.c
+++ fntxt2sql/fntxt2sql-aqb-bal.c
@@ -53,16 +53,31 @@ static int parseAqbBal(char *pchBuffer, Buchung *pBuchung)
 
 		*pchTemp = '\0';
 
+		/* Fields of AqBanking Output
+		 * See chapter 10.4.7 of AqBanking Handbook at
+		 * https://www.aquamaniac.de/sites/download/download.php?package=09&release=09&file=01&dummy=aqbanking4-handbook-20091231.pdf
+			1 "Account" (fester String)
+			2 Bankleitzahl des Kontos
+			3 Kontonummer
+			4 Name der Bank (falls bekannt)
+			5 Name des Kontos (falls bekannt)
+			6 Datum des gebuchten Kontostandes (DD.MM.JJJJ)
+			7 Uhrzeit des gebuchten Kontostandes (hh:mm)
+			8 Betrag des gebuchten Kontostandes
+			9 Waehrung des gebuchten Kontostandes
+			10 Datum des notierten Kontostandes (DD.MM.JJJJ)
+			11 Uhrzeit des notierten Kontostandes (hh:mm)
+			12 Betrag des notierten Kontostandes
+			13 Waehrung des notierten Kontostandes
+		 */
+
 		switch(i)
 		{
 		case 0: // "Account"
 		case 3: // Bank-Name
-		case 4: // Blank
-		case 6: // Time
-		case 9: // Date Repeated
-		case 10: // Time Repeated
-		case 11: // Value 2
-		case 12: // Currency 2
+		case 4: // Account-name (if available)
+		case 6: // Time booked
+		case 10: // Time noted
 			// Diese Felder werden ignoriert.
 			break;
 
@@ -76,23 +91,30 @@ static int parseAqbBal(char *pchBuffer, Buchung *pBuchung)
 			strncpy(&pBuchung->orig_ktonr[0], pchField, sizeof(pBuchung->orig_ktonr)-1);
 			break;
 
-		case 5: // date
+		case 9: // Date noted
+			if(pBuchung->datum[0] != '\0') // got data already from date booked
+				break;
+			// no break!!!
+		case 5: // date booked
 			debug_printf(dbg_fld, "Datum: %s\n", pchField);
-			if(*pchField == '\0')
-				{
-				debug_printf(dbg_fld, "Ignoring Line with empty Datum\n")
-				return RC_EMPTYLINE;
-				}
-
-			strncpy(&pBuchung->datum[0], makeDatum(pchField), sizeof(pBuchung->datum)-1);
+			if(*pchField)
+				strncpy(&pBuchung->datum[0], makeDatum(pchField), sizeof(pBuchung->datum)-1);
 			break;
 
-		case 7: // value_value
+		case 11: // Value noted
+			if(pBuchung->betrag != 0) // got data already from value booked
+				break;
+			// no break!!!
+		case 7: // value booked
 			debug_printf(dbg_fld, "Betrag: %s\n", pchField);
 			pBuchung->betrag = atof(pchField);
 			break;
 
-		case 8: // value_currency
+		case 12: // Currency noted
+			if(pBuchung->waehrung[0] != '\0') // got data already from currency booked
+				break;
+			// no break!!!
+		case 8: // currency booked
 			debug_printf(dbg_fld, "Waehrung: %s\n", pchField);
 			strncpy(&pBuchung->waehrung[0], pchField, sizeof(pBuchung->waehrung)-1);
 			break;
@@ -100,12 +122,18 @@ static int parseAqbBal(char *pchBuffer, Buchung *pBuchung)
 		default:
 			fprintf(stderr, "parsing error: Invalid FieldNr %d (\"%s\")\n", i, pchField);
 			return -5;
-
 		}
 
 		pchTemp++;
 		pchField = pchTemp;
 	}
+
+	if(*pBuchung->datum == '\0') // empty date means to ignore the line
+		{
+		debug_printf(dbg_fld, "Ignoring Line with empty Datum\n")
+		memset(pBuchung, '\0', sizeof(*pBuchung)); // just for safety
+		return RC_EMPTYLINE;
+		}
 
 	return 0;
 }
